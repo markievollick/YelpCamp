e91e26c2f6d468e0711c9d236d91d9d609a4c56f
Markie Vollick <markievollick@gmail.com>
2017-03-14 19:56:01 +0000

    mongo lab

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v10");
+//mongoose.connect("mongodb://localhost/yelp_camp_v10");
+mongoose.connect("mongodb://markie:333@ds047468.mlab.com:47468/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
